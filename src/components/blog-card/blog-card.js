d4aee618c2d6ffb93444740a28e7903a6a10931b
wutong920203 <wutong920203@gmail.com>
2020-03-24 23:30:03 -0400

    update .gitignore

---
+++ src/components/blog-card/blog-card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+
+class BlogCard extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+	// }
+
+	render() {
+		return <div className="blog-card">
+			<div className="blog-card-title">{this.props.title}</div>
+			<div className="blog-card-description">{this.props.description}</div>
+			<div className="blog-card-detail"></div>
+		</div>
+	};
+}
+
+export default BlogCard;
