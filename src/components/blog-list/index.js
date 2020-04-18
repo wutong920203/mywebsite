0ca3a6e0dd35610f288b668f51ce143ca9b86a13
wutong920203 <wutong920203@gmail.com>
2020-04-18 14:59:08 -0400

    add loading state

--- src/components/blog-list/index.js
+++ src/components/blog-list/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Block from '../../elements/block/';
 import BlogCard from "../blog-card/blog-card";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import './style.scss';
 
 class BlogList extends React.Component {
 	render() {
         const blogList = this.props.blogList;
+        let listContent = blogList.length === 0 ? <AiOutlineLoading3Quarters className="loading" /> : blogList.map((article, i) => (
+            <BlogCard key={i} articleDetails={article}/>
+        ));
+
 		return (
             <Block title="文章" containerClassName="blog-list">
                 <div className="blog-list-content">
-                    {
-                        blogList.map((article, i) => (
-                            <BlogCard key={i} articleDetails={article}/>
-                        ))
-                    }
+                    { listContent }
                 </div>
             </Block>
         )
